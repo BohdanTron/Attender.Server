d55404549ab7db2be327d7ad89b3cc59f8b8766f
tronbodya <tronbodya@gmail.com>
2021-03-31 14:01:34 +0300

    added exceptions handling

--- src/Infrastructure/Sms/TwilioSmsService.cs
+++ src/Infrastructure/Sms/TwilioSmsService.cs
@@ -2,14 +2,13 @@
 using Microsoft.Extensions.Options;
 using System.Threading.Tasks;
 using Twilio;
+using Twilio.Exceptions;
 using Twilio.Rest.Verify.V2.Service;
 
 namespace Attender.Server.Infrastructure.Sms
 {
     public class TwilioSmsService : ISmsService
     {
-        //TODO: Add phone number validation
-
         private readonly TwilioSettings _twilioSettings;
 
         public TwilioSmsService(IOptions<TwilioSettings> twilioSettings)
@@ -23,26 +22,38 @@ namespace Attender.Server.Infrastructure.Sms
 
             var serviceSid = _twilioSettings.VerificationServiceSid;
 
-            //TODO: Add exception handling
-            var verification = await VerificationResource.CreateAsync(
-                to: phoneNumber,
-                channel: "sms",
-                pathServiceSid: serviceSid);
+            try
+            {
+                var verification = await VerificationResource.CreateAsync(
+                    to: phoneNumber,
+                    channel: "sms",
+                    pathServiceSid: serviceSid);
 
-            return verification.Status == TwilioConstants.Status.Pending;
+                return verification.Status == TwilioConstants.Status.Pending;
+            }
+            catch (ApiException)
+            {
+                return false;
+            }
         }
 
         public async Task<bool> CheckVerificationCode(string phoneNumber, string code)
         {
             var serviceSid = _twilioSettings.VerificationServiceSid;
 
-            //TODO: Add exception handling
-            var verification = await VerificationCheckResource.CreateAsync(
-                to: phoneNumber,
-                code: code,
-                pathServiceSid: serviceSid);
+            try
+            {
+                var verification = await VerificationCheckResource.CreateAsync(
+                    to: phoneNumber,
+                    code: code,
+                    pathServiceSid: serviceSid);
 
-            return verification.Status == TwilioConstants.Status.Approved;
+                return verification.Status == TwilioConstants.Status.Approved;
+            }
+            catch (ApiException)
+            {
+                return false;
+            }
         }
 
         private void InitTwilioClient()
