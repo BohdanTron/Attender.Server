e19ef7146374837f8333895fd154b8ebef909037
tronbodya <tronbodya@gmail.com>
2021-04-09 23:23:02 +0300

    startup class refactoring

---
+++ src/API/DependencyInjection.cs
@@ -0,0 +1,128 @@
+﻿using Attender.Server.API.Common;
+using Attender.Server.API.Constants;
+using Attender.Server.Infrastructure.Auth;
+using Attender.Server.Infrastructure.Sms;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Security.Claims;
+using System.Text;
+
+namespace Attender.Server.API
+{
+    public static class DependencyInjection
+    {
+        public static void ConfigureOptions(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<TwilioOptions>(configuration.GetSection("Twilio"));
+            services.Configure<AuthOptions>(configuration.GetSection("Auth"));
+        }
+
+        public static void AddAuthentication(this IServiceCollection services, IConfiguration configuration)
+        {
+            var issuer = configuration["Auth:Issuer"];
+            var key = Encoding.ASCII.GetBytes(configuration["Auth:SecurityKey"]);
+
+            var tokenParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidIssuer = issuer,
+                ValidateAudience = false,
+                ValidateLifetime = true,
+                ValidAlgorithms = new[] { SecurityAlgorithms.HmacSha256Signature },
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ClockSkew = TimeSpan.Zero,
+            };
+
+            services.AddSingleton(tokenParameters);
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = true;
+                options.SaveToken = true;
+                options.TokenValidationParameters = tokenParameters;
+            });
+        }
+
+        public static void AddAuthorization(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy(AuthConstants.Policy.RegisteredOnly,
+                    policy => policy.RequireClaim(ClaimsIdentity.DefaultNameClaimType));
+            });
+        }
+
+        public static void AddControllers(this IServiceCollection services)
+        {
+            services.AddControllers(options =>
+            {
+                options.Conventions.Add(new RouteTokenTransformerConvention(new DashParameterTransformer()));
+            })
+            .ConfigureApiBehaviorOptions(options =>
+            {
+                options.InvalidModelStateResponseFactory = context =>
+                {
+                    var errors = context.ModelState.Values
+                        .SelectMany(v => v.Errors)
+                        .Select(e => e.ErrorMessage);
+
+                    return new BadRequestObjectResult(new ErrorResponse(errors));
+                };
+            });
+        }
+
+        public static void AddSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Attender.Server", Version = "v1" });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the Bearer schema",
+                    Name = "Authorization",
+                    Scheme = "Bearer",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header
+                        },
+                        new List<string>()
+                    }
+                });
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+        }
+    }
+}
--- src/API/Startup.cs
+++ src/API/Startup.cs
@@ -1,26 +1,10 @@
-using Attender.Server.API.Common;
-using Attender.Server.API.Constants;
 using Attender.Server.Application;
 using Attender.Server.Infrastructure;
-using Attender.Server.Infrastructure.Auth;
-using Attender.Server.Infrastructure.Sms;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.ApplicationModels;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Security.Claims;
-using System.Text;
 
 namespace Attender.Server.API
 {
@@ -39,96 +23,15 @@ namespace Attender.Server.API
             services.AddApplication();
             services.AddInfrastructure(Configuration);
 
-            services.Configure<TwilioOptions>(Configuration.GetSection("Twilio"));
-            services.Configure<AuthOptions>(Configuration.GetSection("Auth"));
+            services.ConfigureOptions(Configuration);
 
             services.AddRouting(options => options.LowercaseUrls = true);
 
-            var issuer = Configuration["Auth:Issuer"];
-            var key = Encoding.ASCII.GetBytes(Configuration["Auth:SecurityKey"]);
+            services.AddAuthentication(Configuration);
+            services.AddAuthorization(Configuration);
 
-            var tokenParameters = new TokenValidationParameters
-            {
-                ValidateIssuer = true,
-                ValidIssuer = issuer,
-                ValidateAudience = false,
-                ValidateLifetime = true,
-                ValidAlgorithms = new[] { SecurityAlgorithms.HmacSha256Signature },
-                ValidateIssuerSigningKey = true,
-                IssuerSigningKey = new SymmetricSecurityKey(key),
-                ClockSkew = TimeSpan.Zero,
-            };
-
-            services.AddSingleton(tokenParameters);
-
-            services.AddAuthentication(options =>
-            {
-                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            })
-            .AddJwtBearer(options =>
-            {
-                options.RequireHttpsMetadata = true;
-                options.SaveToken = true;
-                options.TokenValidationParameters = tokenParameters;
-            });
-
-            services.AddAuthorization(options =>
-            {
-                options.AddPolicy(AuthConstants.Policy.RegisteredOnly,
-                    policy => policy.RequireClaim(ClaimsIdentity.DefaultNameClaimType));
-            });
-
-            services.AddControllers(options =>
-            {
-                options.Conventions.Add(new RouteTokenTransformerConvention(new DashParameterTransformer()));
-            })
-            .ConfigureApiBehaviorOptions(options =>
-            {
-                options.InvalidModelStateResponseFactory = context =>
-                {
-                    var errors = context.ModelState.Values
-                        .SelectMany(v => v.Errors)
-                        .Select(e => e.ErrorMessage);
-
-                    return new BadRequestObjectResult(new ErrorResponse(errors));
-                };
-            });
-
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Attender.Server", Version = "v1" });
-                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-                {
-                    Description = "JWT Authorization header using the Bearer schema",
-                    Name = "Authorization",
-                    Scheme = "Bearer",
-                    In = ParameterLocation.Header,
-                    Type = SecuritySchemeType.ApiKey
-                });
-                c.AddSecurityRequirement(new OpenApiSecurityRequirement
-                {
-                    {
-                        new OpenApiSecurityScheme
-                        {
-                            Reference = new OpenApiReference
-                            {
-                                Type = ReferenceType.SecurityScheme,
-                                Id = "Bearer"
-                            },
-                            Scheme = "oauth2",
-                            Name = "Bearer",
-                            In = ParameterLocation.Header
-                        },
-                        new List<string>()
-                    }
-                });
-
-                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-                c.IncludeXmlComments(xmlPath);
-            });
+            services.AddControllers();
+            services.AddSwagger();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
