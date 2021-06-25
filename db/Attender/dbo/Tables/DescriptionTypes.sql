b8db743a38578755d3705de90ba8ed8417fb3c19
tronbodya <tronbodya@gmail.com>
2021-06-25 23:46:01 +0300

    adjusted tables

--- db/Attender/dbo/Tables/DescriptionTypes.sql
+++ db/Attender/dbo/Tables/DescriptionTypes.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[DescriptionTypes]
 (
-	[Id] INT NOT NULL,
+	[Id] INT IDENTITY(1, 1) NOT NULL,
     [Type] VARCHAR(50) NOT NULL,
 	
 	CONSTRAINT [PK_DescriptionTypes] PRIMARY KEY CLUSTERED ([Id] ASC),
