789bf0b83411af28f945b64fdb3eed6e8704c8fe
tronbodya <tronbodya@gmail.com>
2021-06-30 22:32:14 +0300

    added automapper unit test

---
+++ tests/Application.UnitTests/Common/Mappings/MappingTests.cs
@@ -0,0 +1,25 @@
+﻿using Attender.Server.Application.Common.Mappings;
+using AutoMapper;
+using Xunit;
+
+namespace Attender.Server.Application.UnitTests.Common.Mappings
+{
+    public class MappingTests
+    {
+        private readonly IConfigurationProvider _configuration;
+
+        public MappingTests()
+        {
+            _configuration = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<MappingProfile>();
+            });
+        }
+
+        [Fact]
+        public void ShouldHaveValidConfiguration()
+        {
+            _configuration.AssertConfigurationIsValid();
+        }
+    }
+}
